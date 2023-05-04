fe7cf163e4f9d594740c824e146ffa049653d4cd
Abdelrahman Adel <abdelrahmanadel203@gmail.com>
2023-05-04 16:50:16 +0300

    Made a call to validateCard function in payOrder function to validate if the card info is valid or not

--- src/PaymentSystem/Ewallet.java
+++ src/PaymentSystem/Ewallet.java
@@ -15,11 +15,16 @@ public class Ewallet extends Payment {
 
     @Override
     public boolean payOrder() {
+        if (authenticator.validateCard(user.getUserCard())) {
+            System.out.println("Card is not valid.");
+            return false;
+        }
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please, enter pin: "); 
+        System.out.println("Please enter pin: "); 
         int input = scanner.nextInt();
         scanner.close();
-        if(input != user.getUserCard().getPin()){
+        if (input != user.getUserCard().getPin()) {
+            System.out.println("Pin is incorrect."); 
             return false;
         }
         return true;
